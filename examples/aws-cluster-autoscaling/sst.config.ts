fdc155d7da8762b9ea537829fea2327706253d16
Jay V <air@live.ca>
2025-05-25 17:37:29 -0400

    docs: edit ex #5021

--- examples/aws-cluster-autoscaling/sst.config.ts
+++ examples/aws-cluster-autoscaling/sst.config.ts
@@ -1,5 +1,84 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+/**
+ * ## AWS Cluster custom autoscaling
+ *
+ * In this example, we'll create a cluster that autoscales based on a custom
+ * metric. In this case, the number of messages in a queue.
+ *
+ * We'll create a queue, and two functions that'll seed and purge the queue. We'll
+ * also create two policies.
+ *
+ * One that scales it up.
+ *
+ * ```ts title="sst.config.ts"
+ * const scaleUpPolicy = new aws.appautoscaling.Policy("ScaleUpPolicy", {
+ *   serviceNamespace: service.nodes.autoScalingTarget.serviceNamespace,
+ *   scalableDimension: service.nodes.autoScalingTarget.scalableDimension,
+ *   resourceId: service.nodes.autoScalingTarget.resourceId,
+ *   policyType: "StepScaling",
+ *   stepScalingPolicyConfiguration: {
+ *     adjustmentType: "ChangeInCapacity",
+ *     cooldown: 5,
+ *     stepAdjustments: [
+ *       {
+ *         metricIntervalLowerBound: "0",
+ *         scalingAdjustment: 1,
+ *       },
+ *     ],
+ *   },
+ * });
+ * ```
+ *
+ * And one that scales it down.
+ *
+ * ```ts title="sst.config.ts"
+ * const scaleDownPolicy = new aws.appautoscaling.Policy("ScaleDownPolicy", {
+ *   serviceNamespace: service.nodes.autoScalingTarget.serviceNamespace,
+ *   scalableDimension: service.nodes.autoScalingTarget.scalableDimension,
+ *   resourceId: service.nodes.autoScalingTarget.resourceId,
+ *   policyType: "StepScaling",
+ *   stepScalingPolicyConfiguration: {
+ *     adjustmentType: "ChangeInCapacity",
+ *     cooldown: 5,
+ *     stepAdjustments: [
+ *       {
+ *         metricIntervalUpperBound: "0",
+ *         scalingAdjustment: -1,
+ *       },
+ *     ],
+ *   },
+ * });
+ * ```
+ *
+ * We'll add a CloudWatch metric alarm that triggers the scaling policies if the
+ * queue depth exceeds 3 messages.
+ *
+ * ```ts title="sst.config.ts"
+ * new aws.cloudwatch.MetricAlarm("QueueDepthAlarm", {
+ *   comparisonOperator: "GreaterThanThreshold",
+ *   evaluationPeriods: 1,
+ *   metricName: "ApproximateNumberOfMessagesVisible",
+ *   namespace: "AWS/SQS",
+ *   period: 10,
+ *   statistic: "Average",
+ *   threshold: 3,
+ *   dimensions: {
+ *     QueueName: queue.nodes.queue.name,
+ *   },
+ *   alarmDescription: "Scale up when queue depth exceeds 3 messages",
+ *   alarmActions: [scaleUpPolicy.arn],
+ *   okActions: [scaleDownPolicy.arn],
+ * });
+ * ```
+ *
+ * To test this example, first deploy your app then:
+ *
+ * 1. Invoke the `MyQueueSeeder` URL. This will cause the service to scale up to 5
+ *    instances in a few minutes.
+ * 2. Then invoke the `MyQueuePurger` URL. This will cause the service to scale
+ *    down to 1 instance in a few minutes.
+ */
 export default $config({
   app(input) {
     return {
@@ -88,7 +167,7 @@ export default $config({
       dimensions: {
         QueueName: queue.nodes.queue.name,
       },
-      alarmDescription: "Scale up when queue depth exceeds 10 messages",
+      alarmDescription: "Scale up when queue depth exceeds 3 messages",
       alarmActions: [scaleUpPolicy.arn],
       okActions: [scaleDownPolicy.arn],
     });
